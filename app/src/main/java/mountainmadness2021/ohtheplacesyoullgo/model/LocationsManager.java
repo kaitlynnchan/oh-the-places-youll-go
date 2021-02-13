3e022f243a1eff5af3ea4c6f384080710938b15f
kaitlynnchan <kaitlynnchan@outlook.com>
2021-02-13 12:45:12 -0800

    Created LocationsManager

---
+++ app/src/main/java/mountainmadness2021/ohtheplacesyoullgo/model/LocationsManager.java
@@ -0,0 +1,30 @@
+package mountainmadness2021.ohtheplacesyoullgo.model;
+
+import java.util.ArrayList;
+
+public class LocationsManager {
+    private Location[] locations = {
+            new Location("Paris", "France"),
+            new Location("Sydney", "Australia"),
+            new Location("Queenstown", "New Zealand"),
+            new Location("Dublin", "Ireland"),
+            new Location("New York City", "USA"),
+            new Location("Maui", "USA"),
+            new Location("Reykjavik", "Iceland"),
+            new Location("Cancun", "Mexico"),
+            new Location("Santorini", "Greece"),
+            new Location("Venice", "Italy"),
+            new Location("Milan", "Italy"),
+            new Location("Bora Bora", "Polynesia"),
+            new Location("Dubai", "United Arabs Emirates"),
+            new Location("Bangkok", "Thailand"),
+            new Location("Bali", "India"),
+            new Location("London", "England"),
+            new Location("New Orleans", "USA"),
+            new Location("Honolulu", "USA"),
+            new Location("Kuala Lumpur", "Malaysia"),
+            new Location("Istanbul", "Turkey"),
+            new Location("Tokyo", "Japan")
+    };
+
+}
